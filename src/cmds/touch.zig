19987b159d68eed18044cd0bb96dfabcb0502f5d
k1ngkevin <90162130+certifiedkaren@users.noreply.github.com>
2026-07-12 15:36:06 +0800

    added reference flag support for touch

--- src/cmds/touch.zig
+++ src/cmds/touch.zig
@@ -9,6 +9,7 @@ pub fn run(io: std.Io, args: []const []const u8) !void {
     var no_create = false;
     var change_modification_time = false;
     var change_access_time = false;
+    var use_reference_file = false;
 
     var positional_start: usize = 0;
 
@@ -19,8 +20,9 @@ pub fn run(io: std.Io, args: []const []const u8) !void {
                     'c' => no_create = true,
                     'm' => change_modification_time = true,
                     'a' => change_access_time = true,
+                    'r' => use_reference_file = true,
                     else => {
-                        std.debug.print("touch: unknown flag -{c}", .{ch});
+                        std.debug.print("touch: unknown flag -{c}\n", .{ch});
                         return;
                     },
                 }
@@ -40,21 +42,43 @@ pub fn run(io: std.Io, args: []const []const u8) !void {
         return;
     }
 
+    if (use_reference_file and positional_args.len < 2) {
+        std.debug.print("touch: missing file operand\n", .{});
+        return;
+    }
+
     const update_access_time = change_access_time or
         (!change_access_time and !change_modification_time);
 
     const update_modification_time = change_modification_time or
         (!change_access_time and !change_modification_time);
 
-    for (positional_args) |arg| {
+    const reference_file_path: []const u8 = if (use_reference_file) positional_args[0] else "";
+
+    const reference_file_stats = if (use_reference_file) std.Io.Dir.cwd().statFile(
+        io,
+        reference_file_path,
+        .{},
+    ) catch |err| switch (err) {
+        error.FileNotFound => {
+            std.debug.print("touch: \"{s}\" : no such file or directory\n", .{reference_file_path});
+            return;
+        },
+        else => return err,
+    } else null;
+
+    const target_args = if (use_reference_file)
+        positional_args[1..]
+    else
+        positional_args;
+
+    for (target_args) |arg| {
         var file = if (no_create) std.Io.Dir.cwd().openFile(io, arg, .{}) catch |err| switch (err) {
-            error.FileNotFound => {
-                return;
-            },
+            error.FileNotFound => continue,
             else => return err,
-        } else std.Io.Dir.cwd().createFile(io, arg, .{}) catch |err| switch (err) {
+        } else std.Io.Dir.cwd().createFile(io, arg, .{ .truncate = false }) catch |err| switch (err) {
             error.FileNotFound => {
-                std.debug.print("touch: \"{s}\" : no such file or directory\n", .{args[0]});
+                std.debug.print("touch: \"{s}\" : no such file or directory\n", .{arg});
                 return;
             },
             else => return err,
@@ -62,8 +86,19 @@ pub fn run(io: std.Io, args: []const []const u8) !void {
         defer file.close(io);
 
         try file.setTimestamps(io, .{
-            .access_timestamp = if (update_access_time) .now else .unchanged,
-            .modify_timestamp = if (update_modification_time) .now else .unchanged,
+            .access_timestamp = if (update_access_time and use_reference_file)
+                std.Io.File.SetTimestamp.init(reference_file_stats.?.atime)
+            else if (update_access_time)
+                .now
+            else
+                .unchanged,
+
+            .modify_timestamp = if (update_modification_time and use_reference_file)
+                .{ .new = reference_file_stats.?.mtime }
+            else if (update_modification_time)
+                .now
+            else
+                .unchanged,
         });
     }
 }
@@ -74,4 +109,5 @@ fn printHelp() void {
     std.debug.print("-c : do not create any files\n", .{});
     std.debug.print("-m : Change modification time of file\n", .{});
     std.debug.print("-a : Change access time of file\n", .{});
+    std.debug.print("-r : Use times of reference file\n", .{});
 }
